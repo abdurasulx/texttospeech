43c80ca131cc6ecf47abeb911427ada15200ecec
abdurasulx <abdurasulx1@gmail.com>
2025-02-25 19:48:17 +0500

    Initial commit

--- main/analys.py
+++ main/analys.py
@@ -9,9 +9,10 @@ def chech_nums(s):
     cs=''
     last=''
     for i in s:
-        if i in ['0','1','2','3','4','5','6','7',"8","9"]:
-            cs=cs+i
-        else:
+        try:
+            cint=int(i)
+            cs+=i
+        except:
             if cs!=''  and i!='.':
                 if dotc==False:
                     full=full+ numtotxt.inttostr (int(cs))
